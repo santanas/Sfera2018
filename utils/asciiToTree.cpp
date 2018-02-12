d57b7fd59075083692c8bb23de7540347ab6ca37
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2018-02-12 17:57:15 +0100

    works on event variables, will add pulseshape

--- utils/asciiToTree.cpp
+++ utils/asciiToTree.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdlib.h>
 
 
 
@@ -19,13 +20,11 @@ int main( int argc, char* argv[] ) {
 
   std::string fileName(argv[1]);
 
-  std::ifstream fs;
-  fs.open(fileName.c_str());
-
   TFile* outfile = TFile::Open( "prova.root", "recreate" );
-  TTree* tree = new TTree( "t", "" );
+  TTree* tree = new TTree( "tree", "" );
 
-  int maxch = 128;
+  int ev;
+  int fcr;
   int nch;
   float base     [128];
   float vamp     [128];
@@ -34,6 +33,8 @@ int main( int argc, char* argv[] ) {
   float tetime   [128];
   float ratecount[128];
 
+  tree->Branch( "ev"       , &ev      , "ev/I"            );
+  tree->Branch( "fcr"      , &fcr     , "fcr/I"           );
   tree->Branch( "nch"      , &nch     , "nch/I"           );
   tree->Branch( "base"     , base     , "base[nch]/F"     );
   tree->Branch( "vamp"     , vamp     , "vamp[nch]/F"     );
@@ -43,29 +44,75 @@ int main( int argc, char* argv[] ) {
   tree->Branch( "ratecount", ratecount, "ratecount[nch]/F");
 
 
+  std::ifstream fs(fileName.c_str());
+
   std::string line;
+  bool wasReadingEvent = false;
+  bool readyForPulseShape = false;
+  int ch = -1;
+
 
   if( fs.good() ) {
 
     while( getline(fs,line) ) {
 
-      //std::string delimiter = " ";
-      //
-      //size_t pos = 0;
-      //std::vector<std::string> words;
-      //std::string word;
-      //while ((pos = line.find(delimiter)) != std::string::npos) {
-      //    word = line.substr(0, pos);
-      //    line.erase(0, pos + delimiter.length());
-      //    words.push_back(word);
-      //}
-      //std::cout << "----" << std::endl;
-      //std::cout << "w[0]: " << words[0] << std::endl;
-      //std::cout << "w[1]: " << words[1] << std::endl;
+      std::string delimiter = " ";
+      
+      size_t pos = 0;
+      std::vector<std::string> words;
+      std::string word;
+      while ((pos = line.find(delimiter)) != std::string::npos) {
+        word = line.substr(0, pos);
+        line.erase(0, pos + delimiter.length());
+        words.push_back(word);
+      }
 
-    }
 
-  }
+      if( words[0]=="===" && words[1]=="EVENT" && wasReadingEvent ) {
+
+        tree->Fill();
+        nch = 0;
+        ch = -1;
+        wasReadingEvent = false;
+
+      } else if( words[0]=="===" && words[1]=="CH:" ) {
+
+        wasReadingEvent = true;
+        readyForPulseShape = true;
+
+        nch += 1;
+
+        ch            = atoi(words[2] .c_str());
+        ev            = atoi(words[4] .c_str());
+        fcr           = atoi(words[6] .c_str());
+        base     [ch] = atof(words[8] .c_str());
+        vamp     [ch] = atof(words[11].c_str());
+        vcharge  [ch] = atof(words[14].c_str());
+        letime   [ch] = atof(words[17].c_str());
+        tetime   [ch] = atof(words[20].c_str());
+        ratecount[ch] = atof(words[23].c_str());
+
+
+      } else if( readyForPulseShape && ch>=0 ) {
+  
+        //myarray = np.fromstring(line, dtype=float, sep=' ')
+        //print(myarray)
+        readyForPulseShape = false;
+   
+      }
+
+    } // while get lines
+
+  } // if file is good
+
+  if( wasReadingEvent )
+    tree->Fill();
+
+
+  fs.close();
+
+  tree->Write();
+  outfile->Close();
 
   return 0;
 

2fdcdf3bf12e8f9acbc96b52c1aa4a7e39f8fe98
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2018-02-13 17:52:00 +0100

    now more intuitive usage message

--- utils/checkPulseShape.cpp
+++ utils/checkPulseShape.cpp
@@ -12,7 +12,7 @@
 int main( int argc, char* argv[] ) {
 
   if( argc!= 4 ) {
-    std::cout << "USAGE: ./checkPulseShape [filename] [event] [channel]" << std::endl;
+    std::cout << "USAGE: ./checkPulseShape [rootFileName] [event] [channel]" << std::endl;
     exit(1);
   }
 
